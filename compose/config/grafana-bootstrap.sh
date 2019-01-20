ceb8e90c3908ba50bf8f1c25b10afc1f544c4509
deniszh <denis.zhdanov@gmail.com>
2019-01-20 19:17:24 +0100

    Adding compose file

---
+++ compose/config/grafana-bootstrap.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+cortex_data_source=$(cat <<EOF
+{
+  "name":"cortex",
+  "type":"prometheus",
+  "url":"http://frontend/api/prom/",
+  "access":"proxy",
+  "isDefault":true,
+  "basicAuth":false
+}
+EOF
+)
+prom_data_source=$(cat <<XUI
+{
+  "name":"Prometheus",
+  "type":"prometheus",
+  "url":"http://prometheus:9090/",
+  "access":"proxy",
+  "isDefault":false,
+  "basicAuth":false
+}
+XUI
+)
+
+until $(curl -sfo /dev/null http://grafana:3000/api/datasources); do
+  # wait for grafana to boot
+  sleep 1
+done
+curl -vX POST -d "${cortex_data_source}" -H "Content-Type: application/json" http://grafana:3000/api/datasources
+curl -vX POST -d "${prom_data_source}" -H "Content-Type: application/json" http://grafana:3000/api/datasources
